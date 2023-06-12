9d782e02924440fbde4a0d78c3e11c16625ae736
saigopi <saigopi4562@gmail.com>
2023-06-12 09:26:21 +0530

    replace

--- backend/backend/src/main/java/com/example/backend/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.backend.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
